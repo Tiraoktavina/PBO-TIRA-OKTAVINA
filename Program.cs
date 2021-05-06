1aee5995518d9b648d8077598feb201006d3d8e2
Tiraoktavina <83747880+Tiraoktavina@users.noreply.github.com>
2021-05-06 14:24:19 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+public class Exercise30 {
+    public static void Main() {
+
+        Console.WriteLine("****************************************************************");
+        Console.WriteLine("Tugas Program Bilangan Desimal Ke Hexadesimal Visual C#");
+        Console.WriteLine("Nama         : Tira Oktavina");
+        Console.WriteLine("Npm          : 1102201012");
+        Console.WriteLine("Kelas        : Malam");
+        Console.WriteLine("Dosen        : Asep Mabrur Aid,S.T,M.Kom");
+        Console.WriteLine("Prodi        : Teknik Informatika");
+        Console.WriteLine("Mata Kuliah  : Pemgraman Berorientasi Objek");
+        Console.WriteLine("****************************************************************");
+        string hexval = "5B0";
+        Console.WriteLine("Hexadecimal number: "+hexval);
+        int decValue = int.Parse(hexval, System.Globalization.NumberStyles.HexNumber);
+        Console.WriteLine("Convert to-");
+        Console.WriteLine("Decimal number: " +decValue);
+    }
+}
